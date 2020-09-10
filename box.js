39a0f4885e550239a3dde11bbb2bd115961f2322
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-09-11 01:46:23 +0530

    Update box.js

--- box.js
+++ box.js
@@ -8,12 +8,12 @@ class Box{
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-      World.add(world,this.box);
+      World.add(world,this.body);
     }
 
   display(){
-      var pos = this.box.position;
-      var angle = this.box.angle;
+      var pos = this.body.position;
+      var angle = this.body.angle;
       push();
       translate(pos.x,pos.y);
       rotate(angle);
