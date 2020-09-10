e8f6d5ba2e5b2ac1dda2b229013ff9f113b5bb65
Saalim Khan <68114437+saalimkhan13@users.noreply.github.com>
2020-09-11 01:40:46 +0530

    Update box.js

--- box.js
+++ box.js
@@ -1,11 +1,11 @@
 class Box{
   constructor(x,y,width,height){
     var options = {
-      'restitution':1.0,
-      'friction': 0.8,
-      'density': 0.8      
+      'restitution':0.8,
+        'friction':0.3,
+        'density':1.0      
     }
-      this.box = Bodies.rectangle(x,y,width,height,options);
+      this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world,this.box);
@@ -22,4 +22,4 @@ class Box{
       rect(0,0,this.width,this.height);
       pop();
     }
-}
+};
